f26ce53f54e0e4aac3260e1628c34e6ddd2d96fa
Louis Manestar <louis.manestar@gmail.com>
2022-02-21 18:49:57 +0000

    Updated script so it updates from the yesterday

--- extractor.py
+++ extractor.py
@@ -4,7 +4,7 @@ import json
 import logging
 import re
 
-from dateutil.parser import parse as parse_date
+from datetime import datetime, date, timedelta
 from newsplease.crawler import commoncrawl_crawler as cc
 
 from botocore.exceptions import ClientError
@@ -15,9 +15,6 @@ WARC_DIRECTORY = os.environ.get("WARC_DIRECTORY")
 ARTICLE_DIRECTORY = os.environ.get("ARTICLE_DIRECTORY")
 
 VALID_HOSTS = json.loads(os.environ.get("VALID_HOSTS"))
-
-CRAWL_START_DATE = os.environ.get("CRAWL_START_DATE")
-CRAWL_END_DATE = os.environ.get("CRAWL_END_DATE")
 
 S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
@@ -46,16 +43,26 @@ def warc_callback(*args):
     pass
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
 
     os.makedirs(WARC_DIRECTORY, exist_ok=True)
     os.makedirs(ARTICLE_DIRECTORY, exist_ok=True)
 
     continue_after_error = ENVIRONMENT.lower() == "prod"
 
+    today = date.today()
+    today_date = datetime(today.year, today.month, today.day)
+
+    end_date = today_date - timedelta(days=1)
+    start_date = end_date - timedelta(days=1)
+
+    logging.info(f"Downloading articles crawled between "
+                 f"{start_date} and {end_date}.")
+
     cc.crawl_from_commoncrawl(
         valid_hosts=VALID_HOSTS,
-        warc_files_start_date=parse_date(CRAWL_START_DATE),
-        warc_files_end_date=parse_date(CRAWL_END_DATE),
+        warc_files_start_date=start_date,
+        warc_files_end_date=end_date,
         callback_on_article_extracted=article_callback,
         callback_on_warc_completed=warc_callback,
         continue_after_error=continue_after_error,
