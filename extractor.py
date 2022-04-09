509d71e69d73e7c0b92cb64a43d456c1e9f516f9
Louis Manestar <louis.manestar@gmail.com>
2022-04-09 21:43:56 +0100

    Added new version of CC-NEWS loader to extractor. Upgraded to python v3.8 since we no longer need news-please and can use better type hinting

--- extractor.py
+++ extractor.py
@@ -5,8 +5,9 @@ import logging
 import re
 
 from datetime import datetime, date, timedelta
-from newsplease.crawler import commoncrawl_crawler as cc
 from botocore.exceptions import ClientError
+
+from loader import CCNewsRecordLoader
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT_TYPE")
 
@@ -48,34 +49,20 @@ def warc_callback(*args):
     pass
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    log_level = logging.DEBUG if ENVIRONMENT != "prod" \
+        else logging.INFO
+
+    logging.basicConfig(level=log_level)
 
     os.makedirs(WARC_DIRECTORY, exist_ok=True)
     os.makedirs(ARTICLE_DIRECTORY, exist_ok=True)
 
-    continue_after_error = ENVIRONMENT.lower() == "prod"
+    end_date = datetime.today()
+    start_date = end_date - timedelta(days=5)
 
-    today = date.today()
-    today_date = datetime(today.year, today.month, today.day)
-
-    end_date = today_date - timedelta(days=1)
-    start_date = end_date - timedelta(days=1)
+    loader = CCNewsRecordLoader(article_callback)
 
     logging.info(f"Downloading articles crawled between "
-                    f"{start_date} and {end_date}.")
-
-    #logging.info(f"Extracting from: \n{'\n\t'.join(VALID_HOSTS)}")
-    logging.info(f"Continuing after error? {continue_after_error}")
-    logging.info(f"warc directory: {WARC_DIRECTORY}")
-    logging.info(f"Article directory: {ARTICLE_DIRECTORY}")
-
-    cc.crawl_from_commoncrawl(
-        valid_hosts=VALID_HOSTS,
-        warc_files_start_date=start_date,
-        warc_files_end_date=end_date,
-        callback_on_article_extracted=article_callback,
-        callback_on_warc_completed=warc_callback,
-        continue_after_error=continue_after_error,
-        local_download_dir_warc=WARC_DIRECTORY,
-        number_of_extraction_processes=1,
-        log_level=logging.INFO)
+                 f"{start_date.date()} and {end_date.date()}.")
+    
+    loader.download_articles(VALID_HOSTS, start_date, end_date)
